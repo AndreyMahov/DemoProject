3bb13a021a4d6c85360c4f1937e8e26eb9641a57
ThunderbirdSPB <puckpuck505@gmail.com>
2022-10-10 11:27:38 +0300

    Refactored Lesson work flow
    * Fixed tests and test data
    * Added group support for lesson crud operations
    * Removed unnecessary controller

--- src/main/java/com/prideTeam/AstonWebSchool/services/crud/impl/LessonCrudServiceImpl.java
+++ src/main/java/com/prideTeam/AstonWebSchool/services/crud/impl/LessonCrudServiceImpl.java
@@ -1,6 +1,8 @@
 package com.prideTeam.AstonWebSchool.services.crud.impl;
 
 import com.prideTeam.AstonWebSchool.entity.Lesson;
+import com.prideTeam.AstonWebSchool.repositories.GroupCrudRepository;
+import com.prideTeam.AstonWebSchool.repositories.LessonCrudRepository;
 import com.prideTeam.AstonWebSchool.repositories.crud.GroupCrudRepository;
 import com.prideTeam.AstonWebSchool.repositories.crud.LessonCrudRepository;
 import com.prideTeam.AstonWebSchool.services.crud.LessonCrudService;
