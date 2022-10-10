1a5a2fdf2184166b540eadc0cff397a716990d88
ThunderbirdSPB <puckpuck505@gmail.com>
2022-10-10 11:27:38 +0300

    Refactored Lesson work flow
    * Fixed tests and test data
    * Added group support for lesson crud operations
    * Removed unnecessary controller

--- src/main/java/com/prideTeam/AstonWebSchool/controllers/group/GroupLessonRestController.java
+++ src/main/java/com/prideTeam/AstonWebSchool/controllers/group/GroupLessonRestController.java
@@ -1,4 +1,4 @@
-package com.prideTeam.AstonWebSchool.controllers.lesson;
+package com.prideTeam.AstonWebSchool.controllers.group;
 
 import com.prideTeam.AstonWebSchool.entity.Lesson;
 import com.prideTeam.AstonWebSchool.services.crud.LessonCrudService;
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,46 +20,52 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
+/**
+ * Контроллер обрабатывающий CRUD запросы для сущности Lesson.
+ * Lesson всегда существует в контексте группы
+ */
+
 @RestController
-@RequestMapping(value = LessonCrudRestController.REST_URL)
-public class LessonCrudRestController {
-    static final String REST_URL = "rest/lessons";
+@RequestMapping(value = GroupLessonRestController.REST_URL)
+public class GroupLessonRestController {
+    static final String REST_URL = "/rest/groups/{groupId}/lessons";
 
     private final LessonCrudService lessonCrudService;
 
-    public LessonCrudRestController(LessonCrudService lessonCrudService) {
+    public GroupLessonRestController(LessonCrudService lessonCrudService) {
         this.lessonCrudService = lessonCrudService;
     }
 
-    public ResponseEntity<Lesson> createWithLocation(@RequestBody Lesson lesson) {
-        Lesson created = lessonCrudService.create(lesson);
+    @PostMapping
+    public ResponseEntity<Lesson> createWithLocation(@RequestBody Lesson lesson, @PathVariable Integer groupId) {
+        Lesson created = lessonCrudService.create(lesson, groupId);
         URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path(REST_URL + "/{id}")
+                .path(REST_URL + "/lessonId")
                 .buildAndExpand(created.getId()).toUri();
         return ResponseEntity.created(uriOfNewResource).body(created);
     }
 
-    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @GetMapping(value = "/{lessonId}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     @ResponseBody
-    public Lesson get(@PathVariable Integer id) {
-        return lessonCrudService.getById(id);
+    public Lesson get(@PathVariable Integer lessonId, @PathVariable Integer groupId) {
+        return lessonCrudService.getByLessonIdAndGroupId(lessonId, groupId);
     }
 
     @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     @ResponseBody
-    public List<Lesson> getAll() {
-        return lessonCrudService.getAll();
+    public List<Lesson> getAll(@PathVariable Integer groupId) {
+        return lessonCrudService.getAll(groupId);
     }
 
-    @PutMapping(value = "/{id}", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @PutMapping(value = "/{lessonId}", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void update(@RequestBody Lesson lesson, @PathVariable Integer id) {
-        lessonCrudService.update(lesson, id);
+    public void update(@RequestBody Lesson lesson, @PathVariable Integer lessonId, @PathVariable Integer groupId) {
+        lessonCrudService.update(lesson, lessonId, groupId);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/{lessonId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Integer id) {
-        lessonCrudService.delete(id);
+    public void delete(@PathVariable Integer lessonId, @PathVariable Integer groupId) {
+        lessonCrudService.delete(lessonId, groupId);
     }
 }
---
+++ src/main/java/com/prideTeam/AstonWebSchool/repositories/LessonCrudRepository.java
@@ -0,0 +1,17 @@
+package com.prideTeam.AstonWebSchool.repositories;
+
+import com.prideTeam.AstonWebSchool.entity.Lesson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface LessonCrudRepository extends JpaRepository<Lesson, Integer> {
+    Optional<Lesson> findByIdAndGroupId(Integer id, Integer groupId);
+
+    List<Lesson> findAllByGroupId(Integer groupId);
+
+    int deleteByIdAndGroupId(Integer lessonId, Integer groupId);
+}
